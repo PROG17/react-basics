8b09c391d2fada83c7513c8eb37c3451fdf45a57
Faisal Mahmud <faisal@willandskill.se>
2019-01-21 11:59:24 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+// Import component
+import MyInputField from "./components/MyInputField";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* Use component to render it here */}
+        <MyInputField />
+      </div>
+    );
+  }
+}
+
+export default App;
